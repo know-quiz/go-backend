3a1fdd81099633f0ee000a1d7ff558f593286033
Dileep Guntamadugu <dileep.guntamadugu@gmail.com>
2024-03-08 22:30:19 +1100

    firestore implementation

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"cloud.google.com/go/firestore"
+	"google.golang.org/api/option"
 )
 
 type Question struct {
@@ -42,14 +46,57 @@ func getQuestions(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(questions)
 }
 
+func getFireStoreQuestions(w http.ResponseWriter, r *http.Request) {
+	var questions []Question
+	ctx := context.Background()
+
+	// Firestore setup
+	// Replace "path/to/your/service-account-file.json" with your service account file path
+	// and "your-project-id" with your GCP project ID.
+
+	d, _ := base64.StdEncoding.DecodeString(os.Getenv("GCP_CREDS_JSON_BASE64"))
+	//sa := option.WithCredentialsFile("/Users/dileep/Downloads/firestore-quiz-app-415922-d27bce37d853.json")
+	client, err := firestore.NewClient(ctx, "quiz-app-415922", option.WithCredentialsJSON(d))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer client.Close()
+
+	// Specify the collection
+	collection := "quiz-questions"
+
+	// Query all documents in the collection
+
+	iter := client.Collection(collection).Documents(ctx)
+	for {
+		var question Question
+		doc, err := iter.Next()
+		if err != nil {
+			break
+		}
+		if err := doc.DataTo(&question); err != nil {
+			log.Printf("Failed to marshal document to struct: %v", err)
+			continue
+		}
+		questions = append(questions, question)
+	}
+
+	// Set Content-Type header
+	w.Header().Set("Content-Type", "application/json")
+	// Encode and return the data as JSON
+	if err := json.NewEncoder(w).Encode(questions); err != nil {
+		http.Error(w, "Error encoding response to JSON", http.StatusInternalServerError)
+	}
+}
+
 func main() {
-	var err error
+	/*var err error
 	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb+srv://app_user:h1yTCK2XApBjAhDu@main.zyalogv.mongodb.net/?retryWrites=true&w=majority&appName=Main"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer client.Disconnect(context.TODO())
-
-	http.HandleFunc("/api/questions", getQuestions)
+	defer client.Disconnect(context.TODO())*/
+	http.HandleFunc("/api/questions", getFireStoreQuestions)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }

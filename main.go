ae9624aa7472d85782bb0a080c876bc99abb3a25
Dileep Guntamadugu <dileep.guntamadugu@gmail.com>
2024-03-12 21:39:42 +1100

    added new routes for post and get user answers

--- main.go
+++ main.go
@@ -22,17 +22,29 @@ type Question struct {
 	Answer   string `json:"answer"`
 }
 
+type QuestionAnswer struct {
+	Question          string `json:"question"`
+	AnsweredCorrectly bool   `json:"answeredCorrectly"`
+}
+
+type UserAnswer struct {
+	UserID            string           `json:"userId"`
+	AnsweredQuestions []QuestionAnswer `json:"answeredQuestions"`
+}
+
+type ApiResponse struct {
+	Message string `json:"message"`
+}
+
+func createFirestoreClient(ctx context.Context) (*firestore.Client, error) {
+	d, _ := base64.StdEncoding.DecodeString(os.Getenv("GCP_CREDS_JSON_BASE64"))
+	return firestore.NewClient(ctx, "quiz-app-415922", option.WithCredentialsJSON(d))
+}
+
 func getFireStoreQuestions(w http.ResponseWriter, r *http.Request) {
 	var questions []Question
 	ctx := context.Background()
-
-	// Firestore setup
-	// Replace "path/to/your/service-account-file.json" with your service account file path
-	// and "your-project-id" with your GCP project ID.
-
-	d, _ := base64.StdEncoding.DecodeString(os.Getenv("GCP_CREDS_JSON_BASE64"))
-	//sa := option.WithCredentialsFile("/Users/dileep/Downloads/firestore-quiz-app-415922-d27bce37d853.json")
-	client, err := firestore.NewClient(ctx, "quiz-app-415922", option.WithCredentialsJSON(d))
+	client, err := createFirestoreClient(ctx)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -67,8 +79,109 @@ func getFireStoreQuestions(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Writes a JSON response
+func writeJsonResponse(w http.ResponseWriter, statusCode int, response ApiResponse) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(response)
+}
+
+func postUserAnswers(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		writeJsonResponse(w, http.StatusMethodNotAllowed, ApiResponse{"Unsupported method"})
+		return
+	}
+
+	var userAnswer UserAnswer
+	// Decode the request body
+	err := json.NewDecoder(r.Body).Decode(&userAnswer)
+	if err != nil {
+		writeJsonResponse(w, http.StatusBadRequest, ApiResponse{err.Error()})
+		return
+	}
+
+	// Basic validation
+	if userAnswer.UserID == "" || len(userAnswer.AnsweredQuestions) == 0 {
+		writeJsonResponse(w, http.StatusBadRequest, ApiResponse{"Missing required fields"})
+		return
+	}
+
+	ctx := context.Background()
+	client, err := createFirestoreClient(ctx)
+	if err != nil {
+		writeJsonResponse(w, http.StatusInternalServerError, ApiResponse{"Error writing to Firestore: " + err.Error()})
+		return
+	}
+	defer client.Close()
+
+	// Start a batch
+	bulkWriter := client.BulkWriter(ctx)
+
+	// Reference to the collection
+	colRef := client.Collection("userAnswers")
+
+	// Loop through answered questions and prepare writes
+	for _, qa := range userAnswer.AnsweredQuestions {
+		docRef := colRef.Doc(userAnswer.UserID).Collection("answeredQuestions").NewDoc()
+		bulkWriter.Set(docRef, qa)
+	}
+	bulkWriter.Flush()
+	writeJsonResponse(w, http.StatusOK, ApiResponse{"Answers recorded successfully"})
+}
+
+func getUserAnswers(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		writeJsonResponse(w, http.StatusMethodNotAllowed, ApiResponse{"Unsupported method"})
+		return
+	}
+
+	// Extract the userId from query parameters
+	userId := r.URL.Query().Get("userId")
+	if userId == "" {
+		writeJsonResponse(w, http.StatusBadRequest, ApiResponse{"Missing userId"})
+		return
+	}
+
+	ctx := context.Background()
+	client, err := createFirestoreClient(ctx)
+	if err != nil {
+		writeJsonResponse(w, http.StatusInternalServerError, ApiResponse{"Error writing to Firestore: " + err.Error()})
+		return
+	}
+	defer client.Close()
+
+	// Firestore query
+	var userAnswers []QuestionAnswer
+	iter := client.Collection("userAnswers").Doc(userId).Collection("answeredQuestions").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err != nil {
+			break
+		}
+		var qa QuestionAnswer
+		if err := doc.DataTo(&qa); err != nil {
+			writeJsonResponse(w, http.StatusInternalServerError, ApiResponse{"Error reading from Firestore: " + err.Error()})
+			return
+		}
+		userAnswers = append(userAnswers, qa)
+	}
+
+	// Check for empty results
+	if len(userAnswers) == 0 {
+		writeJsonResponse(w, http.StatusNotFound, ApiResponse{"No answers found for user"})
+		return
+	}
+
+	// Success response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(userAnswers)
+}
+
 func main() {
 	// Create a new HTTP server
-	http.HandleFunc("/api/questions", getFireStoreQuestions)
+	http.HandleFunc("GET /api/questions", getFireStoreQuestions)
+	http.HandleFunc("POST /api/user/answers", postUserAnswers)
+	http.HandleFunc("GET /api/user/answers", getUserAnswers)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
